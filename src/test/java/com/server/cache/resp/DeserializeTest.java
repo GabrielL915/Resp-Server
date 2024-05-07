562b43c37ff4ef38d596dd1953e40868c379898d
gabriel gomes <gabrielluizgomes1@gmail.com>
2024-05-07 09:51:21 -0300

    test: add error unit test

--- src/test/java/com/server/cache/resp/DeserializeTest.java
+++ src/test/java/com/server/cache/resp/DeserializeTest.java
@@ -1,6 +1,7 @@
 package com.server.cache.resp;
 
 import com.server.cache.domain.models.BulkString;
+import com.server.cache.domain.models.Error;
 import com.server.cache.domain.models.RespType;
 import com.server.cache.domain.models.SimpleString;
 import org.junit.jupiter.api.Test;
@@ -25,5 +26,13 @@ public class DeserializeTest {
         RespType result = RespType.deserialize(input);
         assertInstanceOf(SimpleString.class, result);
         assertEquals("OK", ((SimpleString) result).getMessage());
+    }
+
+    @Test
+    void testDeserializeError() {
+        String input = "-Error message\\r\\n";
+        RespType result = RespType.deserialize(input);
+        assertInstanceOf(Error.class, result);
+        assertEquals("Error message", ((Error) result).getErrorMessage());
     }
 }
