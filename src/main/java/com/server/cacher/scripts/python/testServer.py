27185e4ff33696d6d4b34a46f9f91aa82cc01807
gabriel gomes <gabrielluizgomes1@gmail.com>
2024-07-12 10:43:23 -0300

    fix: resolve simple string parser

--- src/main/java/com/server/cacher/scripts/python/testServer.py
+++ src/main/java/com/server/cacher/scripts/python/testServer.py
@@ -3,18 +3,17 @@ import socket
 def send_command(command):
     host = 'localhost'
     port = 8081
-    
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+
     try:
         sock.connect((host, port))
-        
         sock.sendall(command)
-        
         response = sock.recv(4096)
         print('Received:', response.decode('utf-8'))
-    
+
     finally:
         sock.close()
 
+# Chamando a função
 send_command(b'+OKokokokooko\r\n')
